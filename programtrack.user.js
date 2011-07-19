287d73a771701352e6b0a820751babbf9a48a4f4
Chris Pilkington <chris.pilkington@gmail.com>
2011-07-19 22:19:14 +1000

    Set the default button too.

--- programtrack.user.js
+++ programtrack.user.js
@@ -5,6 +5,15 @@
 // @include       http://canberra.nchsoftware.com:120/codereview?id=*
 // ==/UserScript==
 
+// Set the default rating
 if (document.getElementById('103')) { // Avoid errors
  document.getElementById('103').value = "2$Good. It's what we expect at NCH.";
+}
+
+// Set the default button
+// This element does not have an id set so we hope that it is always
+the first element named "submit"
+var elements = document.getElementsByName("submit");
+if (elements.length != 0) {
+ elements[0].focus();
 }
