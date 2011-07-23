5989869aed0d0925868e1c24f65efd0f2813b88d
Chris Pilkington <chris.pilkington@gmail.com>
2011-07-23 17:36:11 +1000

    Scroll to the top of the page too.

--- programtrack.user.js
+++ programtrack.user.js
@@ -7,13 +7,15 @@
 
 // Set the default rating
 if (document.getElementById('103')) { // Avoid errors
- document.getElementById('103').value = "2$Good. It's what we expect at NCH.";
+  document.getElementById('103').value = "2$Good. It's what we expect at NCH.";
 }
 
 // Set the default button
-// This element does not have an id set so we hope that it is always
-the first element named "submit"
+// This element does not have an id set so we hope that it is always the first element named "submit"
 var elements = document.getElementsByName("submit");
 if (elements.length != 0) {
- elements[0].focus();
+  elements[0].focus();
 }
+
+// Scroll to the top of the window so that we can read through the code review
+scroll(0, 0);
