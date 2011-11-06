968f442b7e4c1b4b3b2a2ab916120ec0a4b87ce0
pilkch <pilkch@breathe.git.sourceforge.net>
2011-11-06 17:27:46 +1100

    Added plain Hacker News user script.

---
+++ plainhackernews.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          Plain Hacker News
+// @namespace     http://chris.iluo.net/userscripts/hackernews
+// @description   Removes the slabs of bright orange
+// @include       http://news.ycombinator.com/*
+// ==/UserScript==
+
+// Change all cells with a background colour of orange to grey
+var elements = document.getElementsByTagName('td');
+for (var i = 0; i < elements.length; i++) {
+  if (elements[i].bgColor == '#ff6600') elements[i].bgColor = '#dddddd';
+}
