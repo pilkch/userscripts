1a8ff402397c247844bb3ec041212eddd2fb8aa4
Chris Pilkington <chris.pilkington@gmail.com>
2012-07-20 20:36:45 +1000

    programtrack Default to Not applicable.

--- programtrack.user.js
+++ programtrack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          ProgramTrack Helper
 // @namespace     http://chris.iluo.net/userscripts/programtrack
-// @description   Pick a default rating of Good for code reviews and make textarea fields larger
+// @description   Pick a default rating of not applicable for code reviews and make textarea fields larger
 // @version       1.0
 // @match         http://canberra.nchsoftware.com/*
 // ==/UserScript==
@@ -12,7 +12,7 @@ var url = document.URL;
 if (url.substring(0, codereview.length) == codereview) {
   // Set the default rating
   var rating = document.getElementById('103');
-  if (rating) rating.value = "2$Good. It's what we expect at NCH.";
+  if (rating) rating.value = "10$Not applicable (not enough to rate)";
 
   // Set the default button
   // This element does not have an id set so we hope that it is always the first element named "submit"
