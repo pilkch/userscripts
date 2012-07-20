e285311130e87599925d1710f4d760278b675127
Chris Pilkington <chris.pilkington@gmail.com>
2012-07-20 19:59:23 +1000

    programtrack Make text fields larger because the defaults are too small to be usable.

--- programtrack.user.js
+++ programtrack.user.js
@@ -1,20 +1,32 @@
 // ==UserScript==
-// @name          ProgramTrack Default Rating
+// @name          ProgramTrack Helper
 // @namespace     http://chris.iluo.net/userscripts/programtrack
-// @description   Pick a default rating of Good for ProgramTrack code reviews
+// @description   Pick a default rating of Good for code reviews and make textarea fields larger
 // @version       1.0
-// @match         http://canberra.nchsoftware.com/codereview?id=*
+// @match         http://canberra.nchsoftware.com/*
 // ==/UserScript==
 
-// Set the default rating
-var rating = document.getElementById('103');
-if (rating) rating.value = "2$Good. It's what we expect at NCH.";
+// Apply fixes for code review pages
+var codereview = "http://canberra.nchsoftware.com:120/codereview?id=";
+var url = document.URL;
+if (url.substring(0, codereview.length) == codereview) {
+  // Set the default rating
+  var rating = document.getElementById('103');
+  if (rating) rating.value = "2$Good. It's what we expect at NCH.";
 
-// Set the default button
-// This element does not have an id set so we hope that it is always the first element named "submit"
-var elements = document.getElementsByName("submit");
-if (elements.length != 0) elements[0].focus();
+  // Set the default button
+  // This element does not have an id set so we hope that it is always the first element named "submit"
+  var elements = document.getElementsByName("submit");
+  if (elements.length != 0) elements[0].focus();
 
-// Scroll to the top of the window so that we can read through the code review
-scroll(0, 0);
-
+  // Scroll to the top of the window so that we can read through the code review
+  scroll(0, 0);
+} else {
+  // Make all textarea fields larger because they are impossibly small
+  var elements = document.getElementsByTagName('textarea');
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    element.setAttribute('rows', 2 * element.getAttribute('rows'));
+    element.setAttribute('cols', 1.2 * element.getAttribute('cols'));
+  }
+}
